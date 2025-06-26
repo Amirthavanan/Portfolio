e21fae11c19ee3770d693e17392b435e927f0c2c
Amirthavanan <amirthavanan123@gmail.com>
2025-06-26 20:13:44 +0530

    commited

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
-const canvas = document.getElementById('particle-canvas');
-const ctx = canvas.getContext('2d');
+// const canvas = document.getElementById('particle-canvas');
+// const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 
-const colors = ['#02488a', '#006acf', '#0587f2', '#78c2ff'];
-const particles = [];
-const numParticles = 80;
+// const colors = ['#02488a', '#006acf', '#0587f2', '#78c2ff'];
+// const particles = [];
+// const numParticles = 80;
 
-window.addEventListener('resize', () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-});
+// window.addEventListener('resize', () => {
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+// });
 
-class Particle {
-  constructor() {
-    this.radius = Math.random() * 3 + 1;
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.color = colors[Math.floor(Math.random() * colors.length)];
-    this.vx = (Math.random() - 0.5) * 0.8;
-    this.vy = (Math.random() - 0.5) * 0.8;
-  }
+// class Particle {
+//   constructor() {
+//     this.radius = Math.random() * 3 + 1;
+//     this.x = Math.random() * canvas.width;
+//     this.y = Math.random() * canvas.height;
+//     this.color = colors[Math.floor(Math.random() * colors.length)];
+//     this.vx = (Math.random() - 0.5) * 0.8;
+//     this.vy = (Math.random() - 0.5) * 0.8;
+//   }
 
-  draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  }
+//   draw() {
+//     ctx.beginPath();
+//     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+//     ctx.fillStyle = this.color;
+//     ctx.fill();
+//   }
 
-  update() {
-    this.x += this.vx;
-    this.y += this.vy;
+//   update() {
+//     this.x += this.vx;
+//     this.y += this.vy;
 
-    // Bounce off edges
-    if (this.x < 0 || this.x > canvas.width) this.vx *= -1;
-    if (this.y < 0 || this.y > canvas.height) this.vy *= -1;
+//     // Bounce off edges
+//     if (this.x < 0 || this.x > canvas.width) this.vx *= -1;
+//     if (this.y < 0 || this.y > canvas.height) this.vy *= -1;
 
-    this.draw();
-  }
-}
+//     this.draw();
+//   }
+// }
 
-function init() {
-  for (let i = 0; i < numParticles; i++) {
-    particles.push(new Particle());
-  }
-}
+// function init() {
+//   for (let i = 0; i < numParticles; i++) {
+//     particles.push(new Particle());
+//   }
+// }
 
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  particles.forEach(p => p.update());
-  requestAnimationFrame(animate);
-}
+// function animate() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   particles.forEach(p => p.update());
+//   requestAnimationFrame(animate);
+// }
 
-init();
-animate();
+// init();
+// animate();
 
 history.scrollRestoration = "manual"; // Prevents browser from restoring scroll position
   window.onload = function() {
